f4ba5a10dd4f00ab54bace37a9743f91fb221e86
Jinah <fw7128@gmail.com>
2020-10-16 16:14:19 -0700

    create product model

---
+++ models/product.js
@@ -0,0 +1,12 @@
+//1
+const mongoose = require('mongoose')
+
+//2
+const productSchema = mongoose.Schema({
+    name: String,
+    price: Number
+
+})
+
+//3
+module.exports = mongoose.model('product', productSchema)
